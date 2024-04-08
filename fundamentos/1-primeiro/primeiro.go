b5e9299ac2e7bf550a667fdf9dfd4ff731cf41c3
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2024-04-08 13:24:51 -0300

    comentarios

--- fundamentos/1-primeiro/primeiro.go
+++ fundamentos/1-primeiro/primeiro.go
@@ -1,9 +1,15 @@
+// Programas executaveis iniciam pelo pacote main
 package main
 
+/*
+os códigos em go são organizados em pacotes
+e para usa-los é necessário declatar um ou varios imports
+*/
 import (
 	"fmt"
 )
 
+// a porta de entrada de um programa em go é a função main
 func main() {
 	fmt.Println("Ola Mundo!")
 }
--- fundamentos/2-comandos/comandos.go
+++ fundamentos/2-comandos/comandos.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Outro programa em %s!", "Go")
+	fmt.Printf("Outro programa em %s!!!!", "Go")
 }
+
+//o comando go get foi descontinuado
+//substituido por go install
+/* para instalar o sql driver usei o comando :
+   go install github.com/go-sql-driver/mysql@latest
+*/
