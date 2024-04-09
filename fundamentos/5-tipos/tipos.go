e3ec865f1f2c00b6a4ec7bb398aafd078e459a43
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2024-04-09 16:28:53 -0300

    boleanos e strings

--- fundamentos/5-tipos/tipos.go
+++ fundamentos/5-tipos/tipos.go
@@ -27,5 +27,29 @@ func main() {
 	//numeros reais (float32 float64)
 	var x float32 = 49.99
 	fmt.Println("O tipo de x é:", reflect.TypeOf(x))
-	fmt.Println("O tipo do literal 49.99 é:", reflect.TypeOf(49.99))
+	fmt.Println("O tipo do literal 49.99 é:", reflect.TypeOf(49.99)) //float64
+
+	//bolean
+	bo := true
+	fmt.Println("O tipo de bo é:", reflect.TypeOf(bo))
+	fmt.Println((!bo))
+
+	//string
+	s1 := "olá meu nome é Doug"
+	fmt.Println(s1 + "!")
+	fmt.Println("O tamanho da string é:", len(s1))
+
+	//string com multiplas linhas
+	s2 := `Olá
+	meu 
+	nome 
+	é
+	Doug`
+	fmt.Println("O tamanho da string é:", len(s2))
+
+	//tipo char não existe em go
+	//quando passamos um unico caractere ele é interpretado como int32 pelo compilador
+	char := 'a'
+	fmt.Println("O tipo char é:", reflect.TypeOf(char))
+
 }
