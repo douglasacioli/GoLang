d2f54a88c1ef564b4e9a25af60543f600f70b75d
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2024-04-09 16:36:09 -0300

    valores default

---
+++ fundamentos/6-tiposdefault/tiposdefault.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b float64
+	var c bool
+	var d string
+	var e *int //ponteiro de inteiro
+
+	fmt.Printf("%v %v %v %q %v", a, b, c, d, e)
+	/*numericos = 0
+	bool = false
+	string = ""
+	*int = null
+	*/
+}
