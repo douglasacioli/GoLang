91e681db2fb6851817ae23d2231246324171b052
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2024-04-09 16:23:32 -0300

    tipos

---
+++ fundamentos/5-tipos/tipos.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+)
+
+func main() {
+	//numeros inteiros
+	fmt.Println(1, 2, 1000)
+	fmt.Println("Literal inteiro é:", reflect.TypeOf(32000))
+
+	//sem sinal (só positivos)... uint8 uint16 uint32 uint64
+	var b byte = 255
+	fmt.Println("O byte é", reflect.TypeOf(b))
+
+	//com sinal... int8 int16 int32 int64
+	i1 := math.MaxInt64
+	fmt.Println("O valor máximo de int é:", i1)
+	fmt.Println("O tipo da variavael i1 é:", reflect.TypeOf(i1))
+
+	var i2 rune = 'a' //representa um mapeamento da tabela unicode (int32)
+	fmt.Println("O rune é:", reflect.TypeOf(i2))
+	fmt.Println(i2)
+
+	//numeros reais (float32 float64)
+	var x float32 = 49.99
+	fmt.Println("O tipo de x é:", reflect.TypeOf(x))
+	fmt.Println("O tipo do literal 49.99 é:", reflect.TypeOf(49.99))
+}
